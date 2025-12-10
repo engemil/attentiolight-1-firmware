6334a7260e3fbd6bc5c6d8ce0e7099e1d93a1d39
engemil <engemil@users.noreply.github.com>
2025-12-10 23:20:00 +0100

    Added custom location for future custom board files

---
+++ fw_al1mb1/boards/AL1_MB1/board.mk
@@ -0,0 +1,9 @@
+# List of all the board related files.
+BOARDSRC = $(BOARDSDIR)/AL1_MB1/board.c
+
+# Required include directories
+BOARDINC = $(BOARDSDIR)/AL1_MB1/
+
+# Shared variables
+ALLCSRC += $(BOARDSRC)
+ALLINC  += $(BOARDINC)
